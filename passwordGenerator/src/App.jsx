c1ae599cb7a460c905485b412eed2d08eaf84a5b
Sunny Singh <sunnysingh706050@gmail.com>
2023-12-09 07:09:50 +0530

    fixes the problem of not matching length and number of characters in password

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     if (numberAllowed) str += "0123456789";
     if (charAllowed) str += "!@#$%^&*()_+";
 
-    for (let i = 1; i < length; i++) {
+    for (let i = 0; i < length; i++) {
       const char = Math.floor(Math.random() * str.length + 1);
       pass += str.charAt(char);
     }
