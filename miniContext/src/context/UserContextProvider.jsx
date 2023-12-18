3ef52bff6f5a9b7a76b41c5e6bc6e95838342c07
sunnysingh100 <sunnysingh706050@gmail.com>
2023-12-19 04:49:59 +0530

    fixes the user and setUser in UserContextProvider.jsx

--- miniContext/src/context/UserContextProvider.jsx
+++ miniContext/src/context/UserContextProvider.jsx
@@ -3,7 +3,7 @@ import {useState} from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = ({children}) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState("");
   return (
     <UserContext.Provider value={{user, setUser}}>
       {children}
