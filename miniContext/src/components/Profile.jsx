4c1bdcb71ad08537a32ab91ecba4344a6c270237
sunnysingh100 <sunnysingh706050@gmail.com>
2023-12-18 06:12:55 +0530

    corrected  the syntax in profile component

--- miniContext/src/components/Profile.jsx
+++ miniContext/src/components/Profile.jsx
@@ -4,11 +4,10 @@ import UserContext from "../context/UserContext";
 function Profile() {
   const {user} = useContext(UserContext);
   if (!user)
-    return (
-      <h1 className="bg-gray-600 text-white text-center mt-3 text-3xl inline-block p-4">
-        Not logged In
-      </h1>
-    );
+    <h1 className="bg-gray-600 text-white text-center mt-3 text-3xl inline-block p-4">
+      Not logged In
+    </h1>;
+
   return <div>Profile : {user.username}</div>;
 }
 
